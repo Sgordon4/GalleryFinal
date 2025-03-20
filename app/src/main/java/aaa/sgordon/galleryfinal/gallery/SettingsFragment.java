015f104451596f4b415c70fd58bdad5b33ba60f1
Sgordon4 <SeanGordonkh@gmail.com>
2025-03-20 12:21:36 -0500

    Working on settings

---
+++ app/src/main/java/aaa/sgordon/galleryfinal/gallery/SettingsFragment.java
@@ -0,0 +1,31 @@
+package aaa.sgordon.galleryfinal.gallery;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreferenceCompat;
+
+import java.util.UUID;
+
+import aaa.sgordon.galleryfinal.R;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+	private UUID directoryUID;
+
+
+	@Override
+	public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+		setPreferencesFromResource(R.xml.dir_settings, rootKey);
+
+
+		final SwitchPreferenceCompat passwordEnabled = (SwitchPreferenceCompat) findPreference("password_enabled");
+		final Preference passwordChange = findPreference("password_change");
+		passwordChange.setDependency("password_enabled");
+
+
+		passwordEnabled.setOnPreferenceChangeListener((preference, newValue) -> {
+		});
+	}
+}
